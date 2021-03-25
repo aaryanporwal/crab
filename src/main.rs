5db0601d3e3e7451455707e46580282a88f8e697
Aaryan <aaryanporwal2233@gmail.com>
2021-03-26 01:58:31 +0530

    Variables & Data Types 📀

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let x: u32 = 16;
+    let y: f32 = 12.4;
+    let a: bool = true;
+    let ch: char = '😺'; // char in rust is 4 bytes
+    let s: &str = "Hello, world! 😺";
+
+    println!("x: {}, y: {}", x , y);
+    println!("a: {}", a);
+    println!("ch: {}\ns: {}", ch, s);
 }
