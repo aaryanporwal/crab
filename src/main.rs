5778ee7255f50cd390e8ba14ffd57bc406bdf196
Aaryan <aaryanporwal2233@gmail.com>
2021-03-26 02:24:04 +0530

    Arrays and Tuples ✅

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
-    let x = 10 + 6;
-    let y = 12.4;
-    let a = true;
-    let ch = '😺'; // char in rust is 4 bytes
-    let s = "Hello, world! 😺";
-
-    println!("x: {}, y: {}", x , y);
-    println!("a: {}", a);
-    println!("ch: {}\ns: {}", ch, s);
+    let my_tup: (u8, char, &str) = (255, 'h', "Hello!");
+    let my_arr: [&str; 3] = [
+        "string1",
+        "second string",
+        "another string"
+    ];
+    let arr2 = ["string1", "string2"];
+    
+    println!("Tuple: ({}, {}, {})", my_tup.0, my_tup.1, my_tup.2);
+    println!("Array: [{}, {}, {})]", my_arr[0], my_arr[1], my_arr[2]);
+    println!("Array2: [{}, {}]", arr2[0], arr2[1]);
 }
