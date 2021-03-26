d2fdd9e70e4b2e4dbb2b756a7e690ad7219c5c8c
Aaryan <aaryanporwal2233@gmail.com>
2021-03-26 11:41:48 +0530

    Every DS is immutable by default 🙅

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 fn main() {
-    // pointers and references
-    let a = 65;
-    let b = &a;
-    let c = *b;
+    let mut x =10;
+    println!("{}", x);
+    x = 12;
+    println!("{}", x);
 
-    let d = &b; // &&a
-
-    println!("{} {} {} {}", a, b , c, d); // Rust automatically dereferences pointers
-
-    println!("{} {} {} {}", a, *b, c, **d); // Output will be same as above
 }
