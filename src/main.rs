e0e63b6094a706ad7e3b51bad7e9b21a145f89fa
Aaryan <aaryanporwal2233@gmail.com>
2021-03-26 13:26:31 +0530

    Yes, pointers are immutable by default too 🦀

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,11 @@
-// This is how you declare a function in rust
-//
-// fn function_name(parameters) -> return type {
-
-//}
-
 fn main() {
     let mut arr = [ 2, 3, 5, 3 ];
-    arr[1] = square(arr[1]);
-    println!("{:?}", arr);
-    
-    arr = [1,2,3,4];
-    arr[1] = square2(arr[1]);
-    println!("{:?}", arr);
 
+    square(&mut arr[1]);
+    println!("{:?}", arr);
 }
 
-fn square(x: u32) -> u32 {
-    return x*x;
-}
-
-fn square2(x: u32) -> u32 {
-    x*x
+fn square(x: &mut u32) {
+    *x = (*x) * (*x)
 }
 
