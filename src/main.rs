8b94fd38cf4feb95422c62769fcaba872986c8f1
Aaryan <aaryanporwal2233@gmail.com>
2021-03-26 15:34:16 +0530

    If/elseif/else 🤔

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 fn main() {
-    let mut arr = [ 2, 3, 5, 3 ];
+    let a = 10;
+    let b = 12;
 
-    square(&mut arr[1]);
-    println!("{:?}", arr);
-}
-
-fn square(x: &mut u32) {
-    *x = (*x) * (*x)
+    if a < b {
+        println!("a < b");
+    } else if b < a {
+        println!("a > b");
+    } else {
+        println!("a = b");
+    }
 }
 
