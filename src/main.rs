9bbba6252a63d5302d9b77b7ba5061f99d85735b
Aaryan <aaryanporwal2233@gmail.com>
2021-03-26 15:53:05 +0530

    Values to variables can be assigned using loop 🎡

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-fn main() {
+fn main() 
+{
     
     // 1. loop
     
-    let mut i=0;
-    loop {
-        println!("{}", i);
-        i += 1;
-        if i>=10 {
-            break
-        }
-    }
+    // Values to variables can be assigned using loops ➰
+    
+    let mut counter = 0;
 
+    let a = loop 
+    {
+        counter += 1;
+
+        if counter >= 10 
+        {
+            break counter * 2;
+        }
+    }; // loop needs to end with a semicolon
+    println!("counter: {}, a: {}", counter, a);
 }
 
