d62a44e2eda7b57db4adeb153e5ff6b725259f30
Aaryan <aaryanporwal2233@gmail.com>
2021-03-26 12:03:21 +0530

    Writing Functions 🏁

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,25 @@
+// This is how you declare a function in rust
+//
+// fn function_name(parameters) -> return type {
+
+//}
+
 fn main() {
-    let mut x =10;
-    println!("{}", x);
-    x = 12;
-    println!("{}", x);
+    let mut arr = [ 2, 3, 5, 3 ];
+    arr[1] = square(arr[1]);
+    println!("{:?}", arr);
+    
+    arr = [1,2,3,4];
+    arr[1] = square2(arr[1]);
+    println!("{:?}", arr);
 
 }
+
+fn square(x: u32) -> u32 {
+    return x*x;
+}
+
+fn square2(x: u32) -> u32 {
+    x*x
+}
+
