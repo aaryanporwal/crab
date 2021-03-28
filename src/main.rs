649b09375818273896c7cc18cf8c5d9874f22d36
Aaryan <aaryanporwal2233@gmail.com>
2021-03-28 16:48:26 +0530

    dot-dot syntax in struct instances can copy fields not initialized from other instances ⚫️⚫️

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,13 @@ fn build_user(username: String, email: String) -> User {
 
 fn main() {
     let user = build_user(String::from("Aaryan"), String::from("aaryan@aaryan.com"));
-    
+    let user2 = User {
+        username: String::from("kamiyaa"),
+        email: String::from("kamiyaa@aaa.com"),
+        ..user // copy all fields from user not initialized in user2     
+    };
+
     println!("{:?}", user);
+    println!("{:?}", user2);
 }
 
